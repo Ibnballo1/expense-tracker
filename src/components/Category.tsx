f968451ea40f414a823aa2b78fd35f4ec72aa4d6
Ibnballo1 <belloabdullateef035@gmail.com>
2024-07-02 10:27:41 +0100

    Add Category component

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,26 +1,65 @@
-interface Props {
-  expenseList: any;
-  total: number;
+import { ChangeEvent, useState } from "react";
+
+interface Expense {
+  id: number;
+  description: string;
+  amount: number;
+  category: string;
 }
 
-function Category(props: Props) {
+interface Props {
+  expenses: Expense[];
+  setExpenses: (expenses: Expense[]) => void;
+}
+
+function Category({ expenses, setExpenses }: Props) {
+  const [selectedExpense, setSelectedExpense] = useState("all");
+
+  let total = 0;
+  const handleOnChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedExpense(e.target.value);
+  };
+
+  const filteredExpense = expenses.filter((expense: Expense) =>
+    selectedExpense === "all"
+      ? expense.category
+      : expense.category === selectedExpense
+  );
+
+  filteredExpense.map((obj) => (total += obj.amount));
+
+  const handleDelete = (id: number) => {
+    setExpenses(expenses.filter((expense) => expense.id !== id));
+  };
+
+  const expenseList = filteredExpense.map((obj: Expense) => (
+    <tr key={obj.id}>
+      <td>{obj.description}</td>
+      <td>${obj.amount}</td>
+      <td>{obj.category}</td>
+      <td>
+        <button onClick={() => handleDelete(obj.id)}>Delete</button>
+      </td>
+    </tr>
+  ));
+
   return (
     <div>
-      <select>
-        <option>Choose a Category</option>
-        <option>All</option>
-        <option>Electricity</option>
-        <option>Utility</option>
+      <select name="expenses" value={selectedExpense} onChange={handleOnChange}>
+        <option value="all">All</option>
+        <option value="Entertainment">Entertainment</option>
+        <option value="Utilities">Utilities</option>
+        <option value="Groceries">Groceries</option>
       </select>
       <table>
         <th>Description</th>
         <th>Amount</th>
         <th>Category</th>
         <th>Delete</th>
-        {props.expenseList}
+        {expenseList}
         <tr>
           <td>Total</td>
-          <td>{props.total}</td>
+          <td>{total}</td>
         </tr>
       </table>
     </div>
